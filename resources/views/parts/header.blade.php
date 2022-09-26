9a5af3a84b66a88116658040674d8dd8ed142018
maiquochuy <huymq.21it@vku.udn.vn>
2022-09-26 08:51:05 +0700

    day len

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -16,9 +16,84 @@
                         <li><a href="{{ route('shop') }}">Shop</a></li>
                         <li><a href="#">Blog</a></li>
                         <li><a href="#">Contact</a></li>
+                        <li class="dropdown">
+                            <a href="#" id="branch">Branch</a>
+                            <div class="dropdown-content">
+                                <div class="row_content">
+                                    <div class="column_content">
+                                    <a href="#" class="column_a">Link 1</a>
+                                    <a href="#" class="column_a">Link 2</a>
+                                    <a href="#" class="column_a">Link 3</a>
+                                    <a href="#" class="column_a">Link 1</a>
+                                    <a href="#" class="column_a">Link 2</a>
+                                    <a href="#" class="column_a">Link 3</a>
+                                    </div>
+                                    <div class="column_content">
+                                    <a href="#" class="column_a">Link 1</a>
+                                    <a href="#" class="column_a">mink 2</a>
+                                    <a href="#" class="column_a">Link 3</a>
+                                    <a href="#" class="column_a">Link 1</a>
+                                    <a href="#" class="column_a">Link 2</a>
+                                    <a href="#" class="column_a">Link 3</a>
+                                    </div>
+                                    <div class="column_content">
+                                    <a href="#" class="column_a">Link 1</a>
+                                    <a href="#" class="column_a">Link 2</a>
+                                    <a href="#" class="column_a">Link 3</a>
+                                    <a href="#" class="column_a">Link 1</a>
+                                    <a href="#" class="column_a">Link 2</a>
+                                    <a href="#" class="column_a">Link 3</a>
+                                    </div>
+                                    <div class="column_content">
+                                    <a href="#" class="column_a">Link 1</a>
+                                    <a href="#" class="column_a">Link 2</a>
+                                    <a href="#" class="column_a">Link 3</a>
+                                    <a href="#" class="column_a">Link 1</a>
+                                    <a href="#" class="column_a">Link 2</a>
+                                    <a href="#" class="column_a">Link 3</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
                     </ul>
                 </nav>
             </div>
+            {{-- <div class="dropdown-content" id="dropdownid">
+                <div class="row_content">
+                    <div class="column_content">
+                    <a href="#" class="column_a">Link 1</a>
+                    <a href="#" class="column_a">Link 2</a>
+                    <a href="#" class="column_a">Link 3</a>
+                    <a href="#" class="column_a">Link 1</a>
+                    <a href="#" class="column_a">Link 2</a>
+                    <a href="#" class="column_a">Link 3</a>
+                    </div>
+                    <div class="column_content">
+                    <a href="#" class="column_a">Link 1</a>
+                    <a href="#" class="column_a">mink 2</a>
+                    <a href="#" class="column_a">Link 3</a>
+                    <a href="#" class="column_a">Link 1</a>
+                    <a href="#" class="column_a">Link 2</a>
+                    <a href="#" class="column_a">Link 3</a>
+                    </div>
+                    <div class="column_content">
+                    <a href="#" class="column_a">Link 1</a>
+                    <a href="#" class="column_a">Link 2</a>
+                    <a href="#" class="column_a">Link 3</a>
+                    <a href="#" class="column_a">Link 1</a>
+                    <a href="#" class="column_a">Link 2</a>
+                    <a href="#" class="column_a">Link 3</a>
+                    </div>
+                    <div class="column_content">
+                    <a href="#" class="column_a">Link 1</a>
+                    <a href="#" class="column_a">Link 2</a>
+                    <a href="#" class="column_a">Link 3</a>
+                    <a href="#" class="column_a">Link 1</a>
+                    <a href="#" class="column_a">Link 2</a>
+                    <a href="#" class="column_a">Link 3</a>
+                    </div>
+                </div>
+            </div> --}}
             <div class="col-lg-3">
                 <div class="header__right">
                     <div class="header__right__auth">
@@ -41,5 +116,20 @@
             <i class="fa fa-bars"></i>
         </div>
     </div>
+    <script>
+        // function addMenu() {
+        //     var menu = document.querySelector('.dropdown-content');
+        //     console.log("add Menu");
+        //     menu.style.display = 'block';
+        // } 
+        // function removeMenu() {
+        //     var menu = document.getElementById('dropdownid');
+        //     var menuitem = document.getElementByC
+        //     console.log("remove menu");
+        //     // menu.style.display = 'none';
+        // }
+        // document.getElementById("branch").onmouseover = function() {addMenu()};
+        // document.getElementById("dropdownid").onmouseout = function() {removeMenu()};
+    </script>
 </header>
 {{-- Header Section End --}}
