7a3cc4d9c9bb30cb91dd4aea9e2ca0b3cb8d5cc7
NV Dung <nvandung296@gmail.com>
2022-11-13 00:46:23 +0700

    update dailyRev

--- app/Models/Revenue.php
+++ app/Models/Revenue.php
@@ -151,10 +151,11 @@ class Revenue extends Model
         $total= ($productMoney+$deliveryCharges)-(float)(($productMoney * $discountPercent) / 100); //calculate total
 
         // update dailyrev, weeklyrev, monthlyrev
+        $nowDay=DB::table($this->weeklyTable)->orderBy('idDRevenue','desc')->limit(1)->get();
         DB::table($this->dailyTable)
         ->where('releaseDate',$nowDate)
         -> update([
-            'revenue' => $total,
+            'revenue' => ($total+$nowDay[0]->revenue),
             'quantity'=>$quantityOfProduct
         ]); // update dailyrev
 
